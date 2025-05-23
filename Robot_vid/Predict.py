6088351d99eb0d854475d690ba4a3038b3b6c8c8
Yin Yu <yinyu.21.tan@gmail.com>
2025-05-23 16:07:11 +0800

    video update

---
+++ Robot_vid/Predict.py
@@ -0,0 +1,278 @@
+from tensorflow.keras.preprocessing import image
+import tensorflow as tf
+import numpy as np
+from robot.api.deco import keyword
+import os
+from robot.api import logger  # Import Robot Framework logger
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(script_dir, '..', 'models', 'multiclass_img2_model_v11.h5')
+MODEL_PATH = os.path.abspath(MODEL_PATH) 
+model = tf.keras.models.load_model(MODEL_PATH, compile=False)
+# MODEL_PATH = "models/multiclass_img2_model_v11.h5"
+# model = tf.keras.models.load_model(MODEL_PATH, compile=False)
+# print(f"Model Loaded: {MODEL_PATH}")
+
+img_size = 224
+class_indices = {'blue': 0, 'cyan': 1, 'faulty': 2, 'green': 3, 'orange': 4, 'pink': 5, 'red': 6, 'white': 7, 'yellow': 8}
+
+def preprocess_img(img):
+    img = image.load_img(img, target_size=(img_size, img_size))
+    img_array = image.img_to_array(img)                              # converts image to Numpy array -> (224,224,3)
+    img_array = img_array / 255.0                                    # normalization
+    img_array = np.expand_dims(img_array, axis=0)                    # adds a batch dimension -> (1, 224, 224, 3)
+    return img_array
+
+# def LoadModel(MODEL_PATH):
+#     model = tf.keras.models.load_model(MODEL_PATH, compile=False)
+#     print(f"Model Loaded: {MODEL_PATH}")
+#     return model
+
+def Predict(model, img, class_indices):
+    try:
+        img_arr = preprocess_img(img)
+        prediction = model.predict(img_arr)                          # returns probabilities for each class (via softmax)
+        prediction_index = np.argmax(prediction[0])                  # returns index of highest probability
+
+        index_to_class = {v: k for k, v in class_indices.items()}    # do opp. mapping for index to colour e.g 0: 'blue' , …
+        predicted_label = index_to_class[prediction_index]           # returns colour name
+
+        # I actually don't really understand how this prediction works
+        # confidence = prediction if prediction > 0.5 else 1 - prediction
+        # print(f"Raw Prediction: {prediction}")
+        # print(f"{prediction_class}--->{confidence * 100:.2f}%")
+        return predicted_label 
+
+    except Exception as e:
+        print(f"Error loading model: {e}")
+        model = None
+
+@keyword("Log Embedded Image")
+def log_embedded_image(image_path):
+    import base64
+    import os
+    from robot.api import logger
+
+    if not os.path.exists(image_path):
+        raise FileNotFoundError(f"Image not found: {image_path}")
+
+    with open(image_path, "rb") as img_file:
+        ext = os.path.splitext(image_path)[1][1:].lower()
+        encoded = base64.b64encode(img_file.read()).decode('utf-8')
+        tag = f'<img src="data:image/{ext};base64,{encoded}" width="300"/>'
+        logger.info(tag, html=True)
+
+
+# @keyword("Predict Directory Colour")
+# def PredictDirectoryColour(model_path, directory, expected_color):
+#     try:
+#         model = LoadModel(MODEL_PATH)
+#         for files in os.listdir(os.path.join(directory)):
+#             img_path = os.path.join(directory, files)
+#             logger.info(f"Reading Image: {img_path}")
+#             print(f"Reading Image:{img_path}")
+#             result = Predict(img_path, class_indices)
+#             logger.info(f"Prediction result: {result}")
+#             if result == expected_color:
+#                 return "PASS"
+#             else:
+#                     # logger.info("All Images passed.")
+#                     logger.error(f"Unexpected result '{result}' for image: {img_path}")
+#                     return "FAIL"
+            
+#     except Exception as e:
+#         logger.error(f"Exception occurred: {str(e)}")
+#         print(f"Error loading model or directory: {e}")
+#         return "FAIL"
+
+@keyword("Predict Directory Green")
+def PredictDirectoryGreen(directory):
+    try:
+        for files in os.listdir(os.path.join(directory)):
+            img_path = os.path.join(directory, files)
+            logger.info(f"Reading Image: {img_path}")
+            print(f"Reading Image:{img_path}")
+            result = Predict(model, img_path, class_indices)
+            logger.info(f"Prediction result: {result}")
+            if result == "green":
+                return "PASS"
+            elif result == "faulty":
+                logger.error(f"Test Failed: {result} is faulty.")
+                return "FAIL"
+            else:
+                    # logger.info("All Images passed.")
+                    logger.error(f"Unexpected result '{result}' for image: {img_path}")
+                    return "FAIL"
+            
+    except Exception as e:
+        logger.error(f"Exception occurred: {str(e)}")
+        print(f"Error loading model or directory: {e}")
+        return "FAIL"
+
+@keyword("Predict Directory Red")
+def PredictDirectoryRed(directory):
+    try:
+        for files in os.listdir(os.path.join(directory)):
+            img_path = os.path.join(directory, files)
+            logger.info(f"Reading Image: {img_path}")
+            print(f"Reading Image:{img_path}")
+            result = Predict(model, img_path, class_indices)
+            logger.info(f"Prediction result: {result}")
+            if result == "red":
+                return "PASS"
+            elif result == "faulty":
+                logger.error(f"Test Failed: {result} is faulty.")
+                return "FAIL"
+            else:
+                    # logger.info("All Images passed.")
+                    logger.error(f"Unexpected result '{result}' for image: {img_path}")
+                    return "FAIL"
+            
+    except Exception as e:
+        logger.error(f"Exception occurred: {str(e)}")
+        print(f"Error loading model or directory: {e}")
+        return "FAIL"
+    
+@keyword("Predict Directory Pink")
+def PredictDirectoryPink(directory):
+    try:
+        for files in os.listdir(os.path.join(directory)):
+            img_path = os.path.join(directory, files)
+            logger.info(f"Reading Image: {img_path}")
+            print(f"Reading Image:{img_path}")
+            result = Predict(model, img_path, class_indices)
+            logger.info(f"Prediction result: {result}")
+            if result == "pink":
+                return "PASS"
+            elif result == "faulty":
+                logger.error(f"Test Failed: {result} is faulty.")
+                return "FAIL"
+            else:
+                    # logger.info("All Images passed.")
+                    logger.error(f"Unexpected result '{result}' for image: {img_path}")
+                    return "FAIL"
+            
+    except Exception as e:
+        logger.error(f"Exception occurred: {str(e)}")
+        print(f"Error loading model or directory: {e}")
+        return "FAIL"
+    
+@keyword("Predict Directory White")
+def PredictDirectoryWhite(directory):
+    try:
+        for files in os.listdir(os.path.join(directory)):
+            img_path = os.path.join(directory, files)
+            logger.info(f"Reading Image: {img_path}")
+            print(f"Reading Image:{img_path}")
+            result = Predict(model, img_path, class_indices)
+            logger.info(f"Prediction result: {result}")
+            if result == "white":
+                return "PASS"
+            elif result == "faulty":
+                logger.error(f"Test Failed: {result} is faulty.")
+                return "FAIL"
+            else:
+                    # logger.info("All Images passed.")
+                    logger.error(f"Unexpected result '{result}' for image: {img_path}")
+                    return "FAIL"
+            
+    except Exception as e:
+        logger.error(f"Exception occurred: {str(e)}")
+        print(f"Error loading model or directory: {e}")
+        return "FAIL"
+    
+@keyword("Predict Directory Cyan")
+def PredictDirectoryCyan(directory):
+    try:
+        for files in os.listdir(os.path.join(directory)):
+            img_path = os.path.join(directory, files)
+            logger.info(f"Reading Image: {img_path}")
+            print(f"Reading Image:{img_path}")
+            result = Predict(model, img_path, class_indices)
+            logger.info(f"Prediction result: {result}")
+            if result == "cyan":
+                return "PASS"
+            elif result == "faulty":
+                logger.error(f"Test Failed: {result} is faulty.")
+                return "FAIL"
+            else:
+                    # logger.info("All Images passed.")
+                    logger.error(f"Unexpected result '{result}' for image: {img_path}")
+                    return "FAIL"
+            
+    except Exception as e:
+        logger.error(f"Exception occurred: {str(e)}")
+        print(f"Error loading model or directory: {e}")
+        return "FAIL"
+    
+@keyword("Predict Directory Blue")
+def PredictDirectoryBlue(directory):
+    try:
+        for files in os.listdir(os.path.join(directory)):
+            img_path = os.path.join(directory, files)
+            logger.info(f"Reading Image: {img_path}")
+            print(f"Reading Image:{img_path}")
+            result = Predict(model, img_path, class_indices)
+            logger.info(f"Prediction result: {result}")
+            if result == "blue":
+                return "PASS"
+            elif result == "faulty":
+                logger.error(f"Test Failed: {result} is faulty.")
+                return "FAIL"
+            else:
+                    # logger.info("All Images passed.")
+                    logger.error(f"Unexpected result '{result}' for image: {img_path}")
+                    return "FAIL"
+            
+    except Exception as e:
+        logger.error(f"Exception occurred: {str(e)}")
+        print(f"Error loading model or directory: {e}")
+        return "FAIL"
+    
+@keyword("Predict Directory Orange")
+def PredictDirectoryOrange(directory):
+    try:
+        for files in os.listdir(os.path.join(directory)):
+            img_path = os.path.join(directory, files)
+            logger.info(f"Reading Image: {img_path}")
+            print(f"Reading Image:{img_path}")
+            result = Predict(model, img_path, class_indices)
+            logger.info(f"Prediction result: {result}")
+            if result == "orange":
+                return "PASS"
+            elif result == "faulty":
+                logger.error(f"Test Failed: {result} is faulty.")
+                return "FAIL"
+            else:
+                    # logger.info("All Images passed.")
+                    logger.error(f"Unexpected result '{result}' for image: {img_path}")
+                    return "FAIL"
+            
+    except Exception as e:
+        logger.error(f"Exception occurred: {str(e)}")
+        print(f"Error loading model or directory: {e}")
+        return "FAIL"
+    
+@keyword("Predict Directory Yellow")
+def PredictDirectoryYellow(directory):
+    try:
+        for files in os.listdir(os.path.join(directory)):
+            img_path = os.path.join(directory, files)
+            logger.info(f"Reading Image: {img_path}")
+            print(f"Reading Image:{img_path}")
+            result = Predict(model, img_path, class_indices)
+            logger.info(f"Prediction result: {result}")
+            if result == "yellow":
+                return "PASS"
+            elif result == "faulty":
+                logger.error(f"Test Failed: {result} is faulty.")
+                return "FAIL"
+            else:
+                    # logger.info("All Images passed.")
+                    logger.error(f"Unexpected result '{result}' for image: {img_path}")
+                    return "FAIL"
+            
+    except Exception as e:
+        logger.error(f"Exception occurred: {str(e)}")
+        print(f"Error loading model or directory: {e}")
+        return "FAIL"
---
+++ Robot_vid/prediction.robot
@@ -0,0 +1,217 @@
+*** Settings ***
+Library      SeleniumLibrary
+Library      OperatingSystem
+Library      Process
+Library      Predict.py
+Library      BlackWidow.py
+
+*** Variables ***
+${BATCH_SCRIPT}      video.bat
+# ${MODEL_PATH}        ${EXECDIR}/models/multiclass_img2_model_v11.h5
+${IMAGE_PATH}        pictures
+
+*** Keywords ***
+Set BlackWidow V4 Keyboard Chroma Green
+    Perform Chroma Test BlackWidow V4 Green
+
+Set BlackWidow V4 Keyboard Chroma Red
+    Perform Chroma Test BlackWidow V4 Red
+
+Set BlackWidow V4 Keyboard Chroma Pink
+    Perform Chroma Test BlackWidow V4 Pink
+
+Set BlackWidow V4 Keyboard Chroma White
+    Perform Chroma Test BlackWidow V4 White
+
+Set BlackWidow V4 Keyboard Chroma Cyan
+    Perform Chroma Test BlackWidow V4 Cyan
+
+Set BlackWidow V4 Keyboard Chroma Blue
+    Perform Chroma Test BlackWidow V4 Blue
+
+Set BlackWidow V4 Keyboard Chroma Orange
+    Perform Chroma Test BlackWidow V4 Orange
+
+Set BlackWidow V4 Keyboard Chroma Yellow
+    Perform Chroma Test BlackWidow V4 Yellow
+
+Check If Chroma Status Is Complete Green
+    ${status}=    Run Keyword And Return Status    Check Status Green
+    Should Be True    ${status}    msg=Chroma Setup Incomplete.
+
+Check If Chroma Status Is Complete Red
+    ${status}=    Run Keyword And Return Status    Check Status Red
+    Should Be True    ${status}    msg=Chroma Setup Incomplete.
+
+Check If Chroma Status Is Complete Pink
+    ${status}=    Run Keyword And Return Status    Check Status Pink
+    Should Be True    ${status}    msg=Chroma Setup Incomplete.
+
+Check If Chroma Status Is Complete White
+    ${status}=    Run Keyword And Return Status    Check Status White
+    Should Be True    ${status}    msg=Chroma Setup Incomplete.
+
+Check If Chroma Status Is Complete Cyan
+    ${status}=    Run Keyword And Return Status    Check Status Cyan
+    Should Be True    ${status}    msg=Chroma Setup Incomplete.
+
+Check If Chroma Status Is Complete Blue
+    ${status}=    Run Keyword And Return Status    Check Status Blue
+    Should Be True    ${status}    msg=Chroma Setup Incomplete.
+
+Check If Chroma Status Is Complete Orange
+    ${status}=    Run Keyword And Return Status    Check Status Orange
+    Should Be True    ${status}    msg=Chroma Setup Incomplete.
+
+Check If Chroma Status Is Complete Yellow
+    ${status}=    Run Keyword And Return Status    Check Status Yellow
+    Should Be True    ${status}    msg=Chroma Setup Incomplete.
+
+Take Pictures Using Webcam
+    Run Process		${CURDIR}/${BATCH_SCRIPT}
+    Sleep    5s
+
+Predict From Directory Green
+    ${result}=    Predict Directory Green   ${CURDIR}/${IMAGE_PATH}   
+    Should Be Equal    ${result}    PASS
+
+Predict From Directory Red
+    ${result}=    Predict Directory Red     ${CURDIR}/${IMAGE_PATH}   
+    Should Be Equal    ${result}    PASS
+
+Predict From Directory Pink
+    ${result}=    Predict Directory Pink     ${CURDIR}/${IMAGE_PATH}   
+    Should Be Equal    ${result}    PASS
+
+Predict From Directory White
+    ${result}=    Predict Directory White     ${CURDIR}/${IMAGE_PATH}   
+    Should Be Equal    ${result}    PASS
+
+Predict From Directory Cyan
+    ${result}=    Predict Directory Cyan     ${CURDIR}/${IMAGE_PATH}   
+    Should Be Equal    ${result}    PASS
+
+Predict From Directory Blue
+    ${result}=    Predict Directory Blue     ${CURDIR}/${IMAGE_PATH}   
+    Should Be Equal    ${result}    PASS
+    
+Predict From Directory Orange
+    ${result}=    Predict Directory Orange     ${CURDIR}/${IMAGE_PATH}   
+    Should Be Equal    ${result}    PASS
+
+Predict From Directory Yellow
+    ${result}=    Predict Directory Yellow     ${CURDIR}/${IMAGE_PATH}   
+    Should Be Equal    ${result}    PASS
+
+Move Image To Color Name
+    [Arguments]    ${color}
+    ${src}=    Set Variable    ${CURDIR}/${IMAGE_PATH}/photo.jpg
+    ${dst}=    Set Variable    ${CURDIR}/${IMAGE_PATH}/${color}.jpg
+    Run Keyword And Continue On Failure    File Should Exist    ${src}
+    Move File    ${src}    ${dst}
+    Set Test Variable     ${IMAGE_FILE}    ${dst}
+
+Log And Embed Image
+    File Should Exist    ${IMAGE_FILE}
+    Log Embedded Image   ${IMAGE_FILE}
+
+List Image Folder
+    ${files}=    List Files In Directory    ${CURDIR}/${IMAGE_PATH}
+    Log Many     Found image files:    ${files}
+
+Delete Image
+    Run Keyword And Ignore Error    File Should Exist    ${IMAGE_FILE}
+    Run Keyword And Ignore Error    Remove File          ${IMAGE_FILE}
+
+*** Test Cases ***
+Test Image Classification Green
+    [Teardown]    Delete Image
+    [Documentation]    Check static green or flag faulty
+    Set BlackWidow V4 Keyboard Chroma Green
+    Check If Chroma Status Is Complete Green
+    Take Pictures Using Webcam
+    Move Image To Color Name    green
+    List Image Folder
+    Log And Embed Image
+    Predict From Directory Green
+
+Test Image Classification Red
+    [Teardown]    Delete Image
+    [Documentation]    Check static red or flag faulty
+    Set BlackWidow V4 Keyboard Chroma Red
+    Check If Chroma Status Is Complete Red
+    Take Pictures Using Webcam
+    Move Image To Color Name    red
+    List Image Folder
+    Log And Embed Image
+    Predict From Directory Red
+
+Test Image Classification Pink
+    [Teardown]    Delete Image
+    [Documentation]    Check static pink or flag faulty
+    Set BlackWidow V4 Keyboard Chroma Pink
+    Check If Chroma Status Is Complete Pink
+    Take Pictures Using Webcam
+    Move Image To Color Name    pink
+    List Image Folder
+    Log And Embed Image
+    Predict From Directory Pink
+
+Test Image Classification White
+    [Teardown]    Delete Image
+    [Documentation]    Check static white or flag faulty
+    Set BlackWidow V4 Keyboard Chroma White
+    Check If Chroma Status Is Complete White
+    Take Pictures Using Webcam
+    Move Image To Color Name    white
+    List Image Folder
+    Log And Embed Image
+    Predict From Directory White
+
+Test Image Classification Cyan
+    [Teardown]    Delete Image
+    [Documentation]    Check static cyan or flag faulty
+    Set BlackWidow V4 Keyboard Chroma Cyan
+    Check If Chroma Status Is Complete Cyan
+    Take Pictures Using Webcam
+    Move Image To Color Name    cyan
+    List Image Folder
+    Log And Embed Image
+    Predict From Directory Cyan
+
+Test Image Classification Blue
+    [Teardown]    Delete Image
+    [Documentation]    Check static blue or flag faulty
+    Set BlackWidow V4 Keyboard Chroma Blue
+    Check If Chroma Status Is Complete Blue
+    Take Pictures Using Webcam
+    Move Image To Color Name    blue
+    List Image Folder
+    Log And Embed Image
+    Predict From Directory Blue
+
+Test Image Classification Orange
+    [Teardown]    Delete Image
+    [Documentation]    Check static orange or flag faulty
+    Set BlackWidow V4 Keyboard Chroma Orange
+    Check If Chroma Status Is Complete Orange
+    Take Pictures Using Webcam
+    Move Image To Color Name    orange
+    List Image Folder
+    Log And Embed Image
+    Predict From Directory Orange
+
+Test Image Classification Yellow
+    [Teardown]    Delete Image
+    [Documentation]    Check static yellow or flag faulty
+    Set BlackWidow V4 Keyboard Chroma Yellow
+    Check If Chroma Status Is Complete Yellow
+    Take Pictures Using Webcam
+    Move Image To Color Name    yellow
+    List Image Folder
+    Log And Embed Image
+    Predict From Directory Yellow
+
+# Test Image Faulty Scenario from phone
+#     [Documentation]    Check static yellow or flag faulty
+#     Predict From Directory Pink
